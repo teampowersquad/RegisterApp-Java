b258d91810206a21475d5524dc11548eb2138710
Kalia Hall <kaliadhall@gmail.com>
2020-10-11 17:50:45 -0500

    Edited active user entity -Kalia

--- src/main/java/edu/uark/registerapp/models/entities/ActiveUserEntity.java
+++ src/main/java/edu/uark/registerapp/models/entities/ActiveUserEntity.java
@@ -17,38 +17,24 @@ import org.hibernate.annotations.GenerationTime;
 @Entity
 @Table(name="activeuser")
 public class ActiveUserEntity {
-	// Variables
-	private final UUID id;
-	private UUID employeeId;
-	private String name;
-	private int classification;
-	private String sessionKey;
-	private LocalDateTime createdOn;
-
-	// Constructor
-	public ActiveUserEntity() {
-		this.id = new UUID(0, 0);
-		this.classification = -1;
-		this.name = StringUtils.EMPTY;
-		this.employeeId = new UUID(0, 0);
-		this.sessionKey = StringUtils.EMPTY;
-    }
-
-	@Id
 	// Create column for Id
+    @Id
     @Column(name="id", updatable = false)
     @GeneratedValue(strategy=GenerationType.AUTO)
-	//Id getter function
+    private final UUID id;
+
 	public UUID getId() {
 		return this.id;
 	}
 
 	// Create column for Employee Id
 	@Column(name = "employeeid")
-	// Employee Id getter and setter functions
+	private UUID employeeId;
+
 	public UUID getEmployeeId() {
 		return this.employeeId;
 	}
+
 	public ActiveUserEntity setEmployeeId(final UUID employeeId) {
 		this.employeeId = employeeId;
 		return this;
@@ -56,10 +42,12 @@ public class ActiveUserEntity {
 
 	// Create column for name
 	@Column(name = "name")
-	// Name getter and setter functions
+	private String name;
+
 	public String getName() {
 		return this.name;
 	}
+
 	public ActiveUserEntity setName(final String name) {
 		this.name = name;
 		return this;
@@ -67,10 +55,12 @@ public class ActiveUserEntity {
 
 	// Create column for classification
 	@Column(name = "classification")
-	// Classification getter and setter functions
+	private int classification;
+
 	public int getClassification() {
 		return this.classification;
 	}
+
 	public ActiveUserEntity setClassification(final int classification) {
 		this.classification = classification;
 		return this;
@@ -78,10 +68,12 @@ public class ActiveUserEntity {
 
 	// Create column for Session Key
 	@Column(name = "sessionkey")
-	// Session Key getter and setter functions
+	private String sessionKey;
+
 	public String getSessionKey() {
 		return this.sessionKey;
 	}
+
 	public ActiveUserEntity setSessionKey(final String sessionKey) {
     	this.sessionKey = sessionKey;
     	return this;
@@ -89,9 +81,17 @@ public class ActiveUserEntity {
 
 	// Create column to track of date employee was Created On
     @Column(name="createdon", insertable=false, updatable = false)
-	@Generated(GenerationTime.INSERT)
-	// Getter function to get what date employee was Created On
+    @Generated(GenerationTime.INSERT)
+	private LocalDateTime createdOn;
 	public LocalDateTime getCreatedOn() {
 		return this.createdOn;
 	}
+
+    public ActiveUserEntity() {
+		this.id = new UUID(0, 0);
+		this.classification = -1;
+		this.name = StringUtils.EMPTY;
+		this.employeeId = new UUID(0, 0);
+		this.sessionKey = StringUtils.EMPTY;
+    }
 }
