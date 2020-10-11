c7f0762cd25b28008aafe64237a28e5459009bc0
Kalia Hall <kaliadhall@gmail.com>
2020-10-11 18:11:27 -0500

    Cleaning up main menu js -Kalia

--- src/main/resources/static/scripts/mainMenu.js
+++ src/main/resources/static/scripts/mainMenu.js
@@ -1,42 +1,39 @@
+// Click event handlers
 document.addEventListener("DOMContentLoaded", function(event) {
-	getStartTransactionActionElement().addEventListener(
-		"click",
-		() => { displayError("Functionality has not yet been implemented."); });
+	getStartTransactionActionElement().addEventListener("click", () => { 
+		displayError("Functionality has not yet been implemented."); 
+	});
 
-	getViewProductsActionElement().addEventListener(
-		"click",
-		() => { window.location.assign("/productListing"); });
+	getViewProductsActionElement().addEventListener("click", () => { 
+		window.location.assign("/productListing"); 
+	});
 
-	getCreateEmployeeActionElement().addEventListener(
-		"click",
-		() => { window.location.assign("/employeeDetail"); });
+	getCreateEmployeeActionElement().addEventListener("click", () => { 
+		window.location.assign("/employeeDetail"); 
+	});
 
-	getProductSalesReportActionElement().addEventListener(
-		"click",
-		() => { displayError("Functionality has not yet been implemented."); });
+	getProductSalesReportActionElement().addEventListener("click", () => { 
+		displayError("Functionality has not yet been implemented."); 
+	});
 
-	getCashierSalesReportActionElement().addEventListener(
-		"click",
-		() => { displayError("Functionality has not yet been implemented."); });
+	getCashierSalesReportActionElement().addEventListener("click", () => { 
+		displayError("Functionality has not yet been implemented."); 
+	});
 });
 
 // Getters and setters
 function getViewProductsActionElement() {
 	return document.getElementById("viewProductsButton");
 }
-
 function getCreateEmployeeActionElement() {
 	return document.getElementById("createEmployeeButton");
 }
-
 function getStartTransactionActionElement() {
 	return document.getElementById("startTransactionButton");
 }
-
 function getProductSalesReportActionElement() {
 	return document.getElementById("productSalesReportButton");
 }
-
 function getCashierSalesReportActionElement() {
 	return document.getElementById("cashierSalesReportButton");
 }
