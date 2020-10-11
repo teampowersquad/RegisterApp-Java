4f5a52cfdc336310f3e1b32b5ca822d5221d5f37
Kalia Hall <kaliadhall@gmail.com>
2020-10-11 11:09:11 -0500

    Add comments to model entities -Kalia

--- src/main/java/edu/uark/registerapp/models/entities/ActiveUserEntity.java
+++ src/main/java/edu/uark/registerapp/models/entities/ActiveUserEntity.java
@@ -17,75 +17,81 @@ import org.hibernate.annotations.GenerationTime;
 @Entity
 @Table(name="activeuser")
 public class ActiveUserEntity {
-    @Id
-    @Column(name="id", updatable = false)
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private final UUID id;
-
-	public UUID getId() {
-		return this.id;
-	}
-
-	@Column(name = "employeeid")
+	// Variables
+	private final UUID id;
 	private UUID employeeId;
-
-	public UUID getEmployeeId() {
-		return this.employeeId;
-	}
-
-	public ActiveUserEntity setEmployeeId(final UUID employeeId) {
-		this.employeeId = employeeId;
-		return this;
-	}
-
-	@Column(name = "name")
 	private String name;
-
-	public String getName() {
-		return this.name;
-	}
-
-	public ActiveUserEntity setName(final String name) {
-		this.name = name;
-		return this;
-	}
-
-	@Column(name = "classification")
 	private int classification;
-
-	public int getClassification() {
-		return this.classification;
-	}
-
-	public ActiveUserEntity setClassification(final int classification) {
-		this.classification = classification;
-		return this;
-	}
-
-	@Column(name = "sessionkey")
 	private String sessionKey;
-
-	public String getSessionKey() {
-		return this.sessionKey;
-	}
-
-	public ActiveUserEntity setSessionKey(final String sessionKey) {
-    	this.sessionKey = sessionKey;
-    	return this;
-    }
-
-    @Column(name="createdon", insertable=false, updatable = false)
-    @Generated(GenerationTime.INSERT)
 	private LocalDateTime createdOn;
-	public LocalDateTime getCreatedOn() {
-		return this.createdOn;
-	}
 
-    public ActiveUserEntity() {
+	// Constructor
+	public ActiveUserEntity() {
 		this.id = new UUID(0, 0);
 		this.classification = -1;
 		this.name = StringUtils.EMPTY;
 		this.employeeId = new UUID(0, 0);
 		this.sessionKey = StringUtils.EMPTY;
     }
+
+	@Id
+	// Create column for Id
+    @Column(name="id", updatable = false)
+    @GeneratedValue(strategy=GenerationType.AUTO)
+	//Id getter function
+	public UUID getId() {
+		return this.id;
+	}
+
+	// Create column for Employee Id
+	@Column(name = "employeeid")
+	// Employee Id getter and setter functions
+	public UUID getEmployeeId() {
+		return this.employeeId;
+	}
+	public ActiveUserEntity setEmployeeId(final UUID employeeId) {
+		this.employeeId = employeeId;
+		return this;
+	}
+
+	// Create column for name
+	@Column(name = "name")
+	// Name getter and setter functions
+	public String getName() {
+		return this.name;
+	}
+	public ActiveUserEntity setName(final String name) {
+		this.name = name;
+		return this;
+	}
+
+	// Create column for classification
+	@Column(name = "classification")
+	// Classification getter and setter functions
+	public int getClassification() {
+		return this.classification;
+	}
+	public ActiveUserEntity setClassification(final int classification) {
+		this.classification = classification;
+		return this;
+	}
+
+	// Create column for Session Key
+	@Column(name = "sessionkey")
+	// Session Key getter and setter functions
+	public String getSessionKey() {
+		return this.sessionKey;
+	}
+	public ActiveUserEntity setSessionKey(final String sessionKey) {
+    	this.sessionKey = sessionKey;
+    	return this;
+    }
+
+	// Create column to track of date employee was Created On
+    @Column(name="createdon", insertable=false, updatable = false)
+	@Generated(GenerationTime.INSERT)
+	// Getter function to get what date employee was Created On
+	public LocalDateTime getCreatedOn() {
+		return this.createdOn;
+	}
 }
--- src/main/java/edu/uark/registerapp/models/entities/EmployeeEntity.java
+++ src/main/java/edu/uark/registerapp/models/entities/EmployeeEntity.java
@@ -17,106 +17,142 @@ import org.hibernate.annotations.GenerationTime;
 import edu.uark.registerapp.commands.employees.helpers.EmployeeHelper;
 import edu.uark.registerapp.models.api.Employee;
 
+// Create entity for table of employees
 @Entity
 @Table(name="employee")
 public class EmployeeEntity {
-    @Id
-    @Column(name="id", updatable = false)
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private final UUID id;
+	// Variables
+	private final UUID id;
+	private int employeeId;
+	private String firstName;
+	private String lastName;
+	private byte[] password;
+	private boolean isActive;
+	private int classification;
+	private UUID managerId;
+	private LocalDateTime createdOn;
 
+	// Constructors
+	public EmployeeEntity() {
+		this.employeeId = -1;
+		this.isActive = false;
+		this.id = new UUID(0, 0);
+		this.classification = -1;
+		this.password = new byte[0];
+		this.managerId = new UUID(0, 0);
+		this.lastName = StringUtils.EMPTY;
+		this.firstName = StringUtils.EMPTY;
+	}
+	public EmployeeEntity(final Employee apiEmployee) {
+    	this.id = new UUID(0, 0);
+		this.isActive = apiEmployee.getIsActive();
+		this.lastName = apiEmployee.getLastName();
+		this.firstName = apiEmployee.getFirstName();
+		this.classification = apiEmployee.getClassification();
+		this.password = EmployeeHelper.hashPassword(apiEmployee.getPassword());
+		this.managerId = (
+			(apiEmployee.getManagerId() != null)
+				? apiEmployee.getManagerId()
+				: new UUID(0, 0));
+	}
+
+	@Id
+	// Create column for Id
+    @Column(name="id", updatable = false)
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	// Id getter function
 	public UUID getId() {
 		return this.id;
 	}
 
+	// Create column for Employee Id
 	@Column(name = "employeeid", insertable = false, updatable = false)
 	@Generated(GenerationTime.INSERT)
-	private int employeeId;
-
+	// Id getter function
 	public int getEmployeeId() {
 		return this.employeeId;
 	}
 
+	// Create column for First Name
 	@Column(name = "firstname")
-	private String firstName;
-
+	// First Name getter and setter function
 	public String getFirstName() {
 		return this.firstName;
 	}
-
 	public EmployeeEntity setFirstName(final String firstName) {
 		this.firstName = firstName;
 		return this;
 	}
 
+	// Create column for Last Name
 	@Column(name = "lastname")
-	private String lastName;
-
+	// Last Name getter and setter functions
 	public String getLastName() {
 		return this.lastName;
 	}
-
 	public EmployeeEntity setLastName(final String lastName) {
 		this.lastName = lastName;
 		return this;
 	}
 
+	// Create column for Password
 	@Column(name = "password")
-	private byte[] password;
 
+	// Password getter and setter functions
 	public byte[] getPassword() {
 		return this.password;
 	}
-
 	public EmployeeEntity setPassword(final byte[] password) {
 		this.password = password;
 		return this;
 	}
 
+	// Create column to check if Active
 	@Column(name = "active")
-	private boolean isActive;
 
+	// Is Active getter and setter functions
 	public boolean getIsActive() {
 		return this.isActive;
 	}
-
 	public EmployeeEntity setIsActive(final boolean isActive) {
 		this.isActive = isActive;
 		return this;
 	}
 
+	// Create column for Classification
 	@Column(name = "classification")
-	private int classification;
 
+	// Classification getter and setter functions
 	public int getClassification() {
 		return this.classification;
 	}
-
 	public EmployeeEntity setClassification(final int classification) {
 		this.classification = classification;
 		return this;
 	}
 
+	// Create column for Manager Id
 	@Column(name = "managerid")
-	private UUID managerId;
 
+	// Manager Id getter and setter functions
 	public UUID getManagerId() {
 		return this.managerId;
 	}
-
 	public EmployeeEntity setManagerId(final UUID managerId) {
 		this.managerId = managerId;
 		return this;
 	}
 
+	// Create column to track when user was Created On
 	@Column(name = "createdon", insertable = false, updatable = false)
 	@Generated(GenerationTime.INSERT)
-	private LocalDateTime createdOn;
 
+	// Getter function to get what date employee was Created On
 	public LocalDateTime getCreatedOn() {
 		return this.createdOn;
 	}
 
+	// Set all variables 
 	public Employee synchronize(final Employee apiEmployee) {
 		this.setIsActive(apiEmployee.getIsActive());
 		this.setLastName(apiEmployee.getLastName());
@@ -130,35 +166,9 @@ public class EmployeeEntity {
 				EmployeeHelper.hashPassword(
 					apiEmployee.getPassword()));
 		}
-
 		apiEmployee.setId(this.getId());
 		apiEmployee.setCreatedOn(this.getCreatedOn());
 		apiEmployee.setEmployeeId(this.getEmployeeId());
-
 		return apiEmployee;
-	}
-
-	public EmployeeEntity() {
-		this.employeeId = -1;
-		this.isActive = false;
-		this.id = new UUID(0, 0);
-		this.classification = -1;
-		this.password = new byte[0];
-		this.managerId = new UUID(0, 0);
-		this.lastName = StringUtils.EMPTY;
-		this.firstName = StringUtils.EMPTY;
-	}
-
-	public EmployeeEntity(final Employee apiEmployee) {
-    	this.id = new UUID(0, 0);
-		this.isActive = apiEmployee.getIsActive();
-		this.lastName = apiEmployee.getLastName();
-		this.firstName = apiEmployee.getFirstName();
-		this.classification = apiEmployee.getClassification();
-		this.password = EmployeeHelper.hashPassword(apiEmployee.getPassword());
-		this.managerId = (
-			(apiEmployee.getManagerId() != null)
-				? apiEmployee.getManagerId()
-				: new UUID(0, 0));
 	}
 }
